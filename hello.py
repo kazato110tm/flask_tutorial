3148349c6f551bcb98cc1e1a07cf83729ea3286b
deguchi <deguchi.k.110@gmail.com>
2020-07-13 22:36:53 +0900

    add some files

--- hello.py
+++ hello.py
@@ -1,18 +1,28 @@
 from flask import Flask, render_template
+import pymysql
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    name = "Hoge"
-    return render_template('hello.html', title='flask test', name=name)
+    db = pymysql.connect(
+        host='localhost',
+        user='root',
+        password='root',
+        db='testdb',
+        charset='utf8',
+        cursorclass=pymysql.cursors.DictCursor,
+    )
 
+    cur = db.cursor()
+    sql = "select * from members"
+    cur.execute(sql)
+    members = cur.fetchall()
 
-@app.route('/good')
-def good():
-    name = "Good"
-    return name
+    cur.close()
+    db.close()
+    return render_template('hello.html', title='flask test', members=members)
 
 
 if __name__ == "__main__":
